7a266cc315e0abfd1a82db13aed4f14593cfcc0e
myrn <okanbatuk@protonmail.com>
2023-05-07 00:52:49 +0300

    Added User model

---
+++ server/src/api/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+/*
+ * User Roles
+ */
+const roles = ["user", "admin"];
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      match:
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 128,
+    },
+    role: {
+      type: String,
+      enum: roles,
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("User", userSchema);
